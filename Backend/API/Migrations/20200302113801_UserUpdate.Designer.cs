33925272cdf59ad7aa9c34fab42c58287ba424d9
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-02 12:46:21 +0100

    Updated migrations to match new models.

--- Backend/API/Migrations/20200302113801_UserUpdate.Designer.cs
+++ Backend/API/Migrations/20200302113801_UserUpdate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace API.Migrations
 {
     [DbContext(typeof(APIContext))]
-    partial class APIContextModelSnapshot : ModelSnapshot
+    [Migration("20200302113801_UserUpdate")]
+    partial class UserUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
