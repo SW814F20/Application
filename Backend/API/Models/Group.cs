53c2e8a770d517b4989700fdf3eb3b329b2792c3
Thue Iversen <thue.v.iversen@gmail.com>
2020-03-04 12:16:18 +0100

    Revert "Initial API used for future develepment and Database configuration."

--- Backend/API/Models/Group.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Models
-{
-    public class Group
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public App[] Applications { get; set; }
-    }
-}
