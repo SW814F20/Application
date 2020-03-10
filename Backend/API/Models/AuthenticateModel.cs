307f44003c64473ea7143103148c2d63ee05c032
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-10 11:31:23 +0100

    Updated models to facilitate the UserController

---
+++ Backend/API/Models/AuthenticateModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.User
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- Backend/API/Models/UpdateModel.cs
+++ Backend/API/Models/UpdateModel.cs
@@ -1,10 +1,10 @@
 namespace API.Models.User
 {
-  public class UserModel
+  public class UpdateModel
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
