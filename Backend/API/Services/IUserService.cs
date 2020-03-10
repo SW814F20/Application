dd70010bac8091ebdd3a1414149af2fcaf6ad3ca
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-10 11:36:08 +0100

    Added a UserService

---
+++ Backend/API/Services/IUserService.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using API.Entities;
+
+namespace API.Services
+{
+    // Interface for the UserService
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+    }
+}
