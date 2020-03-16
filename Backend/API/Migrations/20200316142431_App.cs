5e6425ee9f3d0db34a90eeb24b1a2299a0d013cc
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-16 15:49:22 +0100

    Added migration with Apps

---
+++ Backend/API/Migrations/20200316142431_App.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace API.Migrations
+{
+    public partial class App : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    AppName = table.Column<string>(nullable: true),
+                    AppUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apps", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apps");
+        }
+    }
+}
