994f1dee1038ac4fef31425ea09f1a12677d0fbe
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-16 15:47:08 +0100

    Added App entity

---
+++ Backend/API/Entities/App.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class App
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public string AppUrl { get; set; }
+    }
+}
