aa72b262839d71e3df7a98f1cac72fd2470f4cb8
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-21 16:45:35 +0100

    Added migration with new AppColor

---
+++ Backend/API/Migrations/20200321145444_AppColor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AppColor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppColor",
+                table: "Apps",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppColor",
+                table: "Apps");
+        }
+    }
+}
