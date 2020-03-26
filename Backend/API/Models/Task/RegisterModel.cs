291e111f4cd34cd69fb7d73e5b702d1c4fb144ff
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-03-26 14:54:31 +0100

    Added task models

---
+++ Backend/API/Models/Task/RegisterModel.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Task
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int AppId { get; set; }
+        [Required]
+        public List<int> ScreenId { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
