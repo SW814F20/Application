c62c0a453723e2ffeb6f2f7c9ad0d224904555df
Morten Hartvigsen <m.hartvigsen@gmail.com>
2020-06-15 01:54:43 +0200

    Forgot to export important classes

--- testEngine/lib/testEngine.dart
+++ testEngine/lib/testEngine.dart
@@ -6,3 +6,5 @@ library testEngine;
 export 'src/testEngine_base.dart';
 export 'src/UISpec.dart';
 export 'src/UIElement.dart';
+export 'src/CodeGenerators/flutter/flutterTestGenerator.dart';
+export 'src/CodeGenerators/packageExporter.dart';
