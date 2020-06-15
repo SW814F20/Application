e7486cc314a2c3e6f00dda7835c355247c6d88e0
Morten Hartvigsen <m.hartvigsen@gmail.com>
2020-06-15 02:22:14 +0200

    One more important export

--- testEngine/lib/testEngine.dart
+++ testEngine/lib/testEngine.dart
@@ -8,3 +8,4 @@ export 'src/UISpec.dart';
 export 'src/UIElement.dart';
 export 'src/CodeGenerators/flutter/flutterTestGenerator.dart';
 export 'src/CodeGenerators/packageExporter.dart';
+export 'src/providers/sw814Api.dart';
